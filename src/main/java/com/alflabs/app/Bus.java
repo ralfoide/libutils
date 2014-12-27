c7c044595bee349eec0437b85c744f6406226d54
ralf <ralfoide@gmail.com>
2014-12-26 21:03:57 -0800

    LU: self chaining in Bus API, better Bus javadoc.

--- src/main/java/com/alflabs/app/Bus.java
+++ src/main/java/com/alflabs/app/Bus.java
@@ -47,6 +47,34 @@ import com.alflabs.utils.Utils;
  *   without holding the bus. <br/>
  * - Use Bus.getSender() to get a sender object. This one also maintains a weak reference
  *   on the bus. <br/>
+ * <p/>
+ * Example of typical usage:
+ * <pre>
+ *  Globals:
+ *      private static final Bus mGlobalBus = Buses.newBus();
+ *      public static Bus getBus() { return mGlobalBus; }
+ *  Activity:
+ *      public class MyActivity extends Activity implements IBusListener {
+ *         private final Bus.Reference mBusRef = Globals.getBus().getReference().register(this);
+ *         private final Bus.Sender mBusSender = mBusRef.getSender();
+ *         ...
+ *         mBusSender.safeSend(...)
+ *         ...
+ *         void onDestroy() { mBusRef.unregister(this); }
+ *
+ *         // remember that onBusMessage is invoked on the thread of the sender
+ *         // which may not be the UI thread so always use runOnUiThread or similar.
+ *         void onBusMessage(final int what, @Null final Object object) {
+ *           runOnUiThread(new Runnable() {
+ *             public void run() {
+ *               switch(what) {
+ *               // process messages
+ *               }
+ *             }
+ *           }
+ *         }
+ *      }
+ * </pre>
  */
 public class Bus {
 
@@ -117,10 +145,14 @@ public class Bus {
     /**
      * Registers a BusListener with no class filter.
      *
-     * @param listener A receiver object implement {@link IBusListener} or {@link BusAdapter}.
+     * @param listener A receiver object implement {@link com.alflabs.app.IBusListener}
+     *                 or {@link com.alflabs.app.BusAdapter}.
+     * @return self for chaining.
      */
-    public void register(IBusListener listener) {
+    @NonNull
+    public Bus register(IBusListener listener) {
         _addListener(null, listener);
+        return this;
     }
 
     /**
@@ -129,10 +161,14 @@ public class Bus {
      * @param classFilter The object class to filter. Can be null to receive everything (any kind
      *      of object, including null) or the {@link Void} class to filter on message with null
      *      objects.
-     * @param listener A receiver object implementing {@link IBusListener} or {@link BusAdapter}.
+     * @param listener A receiver object implementing {@link com.alflabs.app.IBusListener}
+     *                 or {@link com.alflabs.app.BusAdapter}.
+     * @return self for chaining.
      */
-    public void register(@Null final Class<?> classFilter, final IBusListener listener) {
+    @NonNull
+    public Bus register(@Null final Class<?> classFilter, final IBusListener listener) {
         _addListener(classFilter, listener);
+        return this;
     }
 
     private void _addListener(@Null Class<?> filter, @NonNull IBusListener listener) {
@@ -166,8 +202,15 @@ public class Bus {
         }
     }
 
-    public void unregister(@NonNull IBusListener listener) {
-        if (listener == null) return;
+    /**
+     * Unregisters the listener from the bus.
+     *
+     * @param listener A listener previously registered with the bus.
+     * @return self for chaining.
+     */
+    @NonNull
+    public Bus unregister(@NonNull IBusListener listener) {
+        if (listener == null) return this;
 
         synchronized (mListeners) {
             for (List<WeakReference<IBusListener>> list : mListeners.values()) {
@@ -182,6 +225,8 @@ public class Bus {
                 }
             }
         }
+
+        return this;
     }
 
     // -----
@@ -190,6 +235,7 @@ public class Bus {
      * Returns a weak reference on the Bus that can be used to safely use the
      * Bus even if the Bus reference has been disposed.
      */
+    @NonNull
     public Reference getReference() {
         return new Reference(this);
     }
@@ -198,6 +244,7 @@ public class Bus {
      * Helper to safely use a weak bus reference.
      */
     public static class Reference {
+        @NonNull
         private WeakReference<Bus> mBusRef;
 
         private Reference(@NonNull Bus bus) {
@@ -215,16 +262,17 @@ public class Bus {
         /**
          * Registers a BusListener with no class filter.
          *
-         * @param listener A receiver object implement {@link IBusListener} or {@link BusAdapter}.
-         * @return true if the Bus reference is still valid and the action was done.
+         * @param listener A receiver object implement {@link com.alflabs.app.IBusListener}
+         *                 or {@link com.alflabs.app.BusAdapter}.
+         * @return self for chaining.
          */
-        public boolean register(@NonNull IBusListener listener) {
+        @NonNull
+        public Reference register(@NonNull IBusListener listener) {
             Bus b = getBus();
             if (b != null) {
                 b.register(null, listener);
-                return true;
             }
-            return false;
+            return this;
         }
 
         /**
@@ -233,25 +281,31 @@ public class Bus {
          * @param classFilter The object class to filter. Can be null to receive everything (any kind
          *                    of object, including null) or the {@link Void} class to filter on message with null
          *                    objects.
-         * @param listener    A receiver object implementing {@link com.alflabs.app.IBusListener} or {@link com.alflabs.app.BusAdapter}.
-         * @return true if the Bus reference is still valid and the action was done.
+         * @param listener    A receiver object implementing {@link IBusListener} or {@link BusAdapter}.
+         * @return self for chaining.
          */
-        public boolean register(@Null Class<?> classFilter, @NonNull IBusListener listener) {
+        @NonNull
+        public Reference register(@Null Class<?> classFilter, @NonNull IBusListener listener) {
             Bus b = getBus();
             if (b != null) {
                 b.register(classFilter, listener);
-                return true;
             }
-            return false;
+            return this;
         }
 
-        public boolean unregister(@NonNull IBusListener listener) {
+        /**
+         * Unregisters the listener from the bus.
+         *
+         * @param listener A listener previously registered with the bus.
+         * @return self for chaining.
+         */
+        @NonNull
+        public Reference unregister(@NonNull IBusListener listener) {
             Bus b = getBus();
             if (b != null) {
                 b.unregister(listener);
-                return true;
             }
-            return false;
+            return this;
         }
 
         /**
@@ -275,11 +329,13 @@ public class Bus {
      * Returns a sender that can be used to safely send messages on the Bus
      * even if the Bus reference has been disposed.
      */
+    @NonNull
     public Sender getSender() {
         return new Sender(this);
     }
 
     public static class Sender {
+        @NonNull
         private WeakReference<Bus> mBusRef;
 
         private Sender(@NonNull Bus bus) {
@@ -294,23 +350,29 @@ public class Bus {
             return mBusRef.get();
         }
 
-        public void safeSend(int what) {
-            this.safeSend(what, null);
+        @NonNull
+        public Sender safeSend(int what) {
+            safeSend(what, null);
+            return this;
         }
 
-        public void safeSend(@Null Object object) {
-            this.safeSend(NO_WHAT, object);
+        @NonNull
+        public Sender safeSend(@Null Object object) {
+            safeSend(NO_WHAT, object);
+            return this;
         }
 
-        public void safeSend(int what, @Null Object object) {
+        @NonNull
+        public Sender safeSend(int what, @Null Object object) {
             if (mBusRef == null) {
-                return;
+                return this;
             }
 
             Bus bus = mBusRef.get();
             if (bus != null) {
                 bus._send(what, object);
             }
+            return this;
         }
     }
 
