bdb88bfcbb39d2636c60aef7ebd29b22b9a59d2f
ralf <ralfoide@gmail.com>
2018-02-19 21:31:04 -0800

    LUv2: Fake FileOps for testing (with their own tests)

---
+++ LibUtilsJava/src/test/java/com/alflabs/utils/FakeFileOpsTest.java
@@ -0,0 +1,71 @@
+/*
+ * Project: Lib Utils
+ * Copyright (C) 2018 alf.labs gmail com,
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.alflabs.utils;
+
+import com.google.common.base.Charsets;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static com.alflabs.utils.AssertException.assertException;
+import static com.google.common.truth.Truth.assertThat;
+
+
+public class FakeFileOpsTest {
+    private FileOps mOps;
+
+    @Before
+    public void setUp() throws Exception {
+        mOps = new FakeFileOps();
+    }
+
+    @Test
+    public void testMissingFile() throws Exception {
+        File f = new File("some_file");
+
+        assertThat(mOps.isFile(null)).isFalse();
+        assertThat(mOps.isFile(f)).isFalse();
+
+        assertException(
+                FileNotFoundException.class,
+                () -> mOps.toString(f, Charsets.UTF_8));
+
+        assertException(
+                FileNotFoundException.class,
+                () -> mOps.getProperties(f));
+        assertException(
+                FileNotFoundException.class,
+                () -> mOps.readBytes(f));
+    }
+
+    @Test
+    public void testWriteFile() throws Exception {
+        File f = new File("some_file");
+        byte[] actual = "File=Content".getBytes(Charsets.UTF_8);
+
+        mOps.writeBytes(actual, f);
+
+        assertThat(mOps.isFile(f)).isTrue();
+        assertThat(mOps.toString(f, Charsets.UTF_8)).isEqualTo("File=Content");
+        assertThat(mOps.readBytes(f)).isEqualTo(actual);
+        assertThat(mOps.getProperties(f)).containsExactly("File", "Content");
+    }
+}
