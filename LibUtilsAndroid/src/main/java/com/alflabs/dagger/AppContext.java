77fdd43a18c3f911545c822cd325899327e95997
ralf <ralfoide@gmail.com>
2020-04-24 00:21:02 -0700

    LUv3: Add android dagger AppContext.

---
+++ LibUtilsAndroid/src/main/java/com/alflabs/dagger/AppContext.java
@@ -0,0 +1,31 @@
+/*
+ * Project: Lib Utils
+ * Copyright (C) 2020 alf.labs gmail com,
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.alflabs.dagger;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/** Indicates that a Context is the ApplicationContext. */
+@Qualifier
+@Documented
+@Retention(RUNTIME)
+public @interface AppContext {}
